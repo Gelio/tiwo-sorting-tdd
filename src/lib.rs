862b4db40cbe306ad7d200f148e98017efa37657
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-05-22 09:40:19 +0200

    (cycle 1) Add a test for 2 unsorted elements

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,15 @@
 pub fn insertion_sort(_arr: &mut Vec<i32>) {}
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_should_swap_two_unsorted_elements() {
+        let mut arr = vec![2, 1];
+
+        insertion_sort(&mut arr);
+
+        assert_eq!(arr, vec![1, 2]);
+    }
+}
