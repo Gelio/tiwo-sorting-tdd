9c7071cf0b57983169af93e9ae3e39e9c65cec81
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-05-23 09:59:11 +0200

    (cycle 4) Add support for sorting any orderable value
    The tests do not compile without the code changes, hence the test and
    the implementation change are done in a single commit.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 /// Sorts the array in-place to be in the ascending order.
-pub fn insertion_sort(arr: &mut Vec<i32>) {
+pub fn insertion_sort(arr: &mut Vec<impl Ord>) {
     for index_to_sort in 1..arr.len() {
-        let v = arr[index_to_sort];
+        let v = &arr[index_to_sort];
 
         (0..index_to_sort)
-            .find(|&i| arr[i] >= v)
+            .find(|&i| arr[i].ge(v))
             .and_then(|index_to_insert_at| {
                 arr[index_to_insert_at..=index_to_sort].rotate_right(1);
 
@@ -28,7 +28,7 @@ mod tests {
 
     #[test]
     fn it_should_work_for_empty_vectors() {
-        let mut arr = vec![];
+        let mut arr: Vec<()> = vec![];
 
         insertion_sort(&mut arr);
 
@@ -42,5 +42,57 @@ mod tests {
         insertion_sort(&mut arr);
 
         assert_eq!(arr, vec![-1, 1, 2, 3, 5, 8, 10]);
+    }
+
+    #[derive(PartialEq, Eq, Debug)]
+    struct MyNode {
+        /// The key to sort by
+        key: i32,
+        metadata: i32,
+    }
+
+    impl PartialOrd for MyNode {
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            self.key.partial_cmp(&other.key)
+        }
+    }
+
+    impl Ord for MyNode {
+        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+            self.key.cmp(&other.key)
+        }
+    }
+
+    #[test]
+    fn it_should_sort_any_vector_which_implements_ord() {
+        let mut arr = vec![
+            MyNode {
+                key: 1,
+                metadata: 1,
+            },
+            MyNode {
+                key: 5,
+                metadata: 2,
+            },
+            MyNode {
+                key: 1,
+                metadata: 3,
+            },
+            MyNode {
+                key: 8,
+                metadata: 4,
+            },
+            MyNode {
+                key: 1,
+                metadata: 5,
+            },
+        ];
+
+        insertion_sort(&mut arr);
+
+        assert_eq!(
+            arr.into_iter().map(|node| node.key).collect::<Vec<i32>>(),
+            vec![1, 1, 1, 5, 8],
+        )
     }
 }
